5efb2ccd82966e25ae6297e91e7d15ab07cb4da1
Emmanuel <Emmanuelagyeikumi@gmail.com>
2023-12-23 13:28:38 +0000

    Login complete

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import React, {useEffect, useState} from 'react'
+import { Outlet, Link,useNavigate } from "react-router-dom";
+import { reactLocalStorage } from 'reactjs-localstorage';
 
 export const Layout = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+
+    reactLocalStorage.set('login', false);
+    navigate('/login');
+    // console.log('Logout');
+  };
+
+  const login = reactLocalStorage.get('login');
+  useEffect(() => {
+    // console.log(login)
+    if (login === undefined || login === "false") {
+      handleLogout()
+    }
+  });
+
+
   return (
     <>
       <nav>
